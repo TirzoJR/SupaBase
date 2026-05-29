bf469170d636e585e7a6880ee76d0a2cb36b1fdd
tirzomartinez2003@gmail.com <tirzomartinez2003@gmail.com>
2026-05-28 20:48:10 -0600

    feat: migrando codigo de supabase a develop

--- src/modules/bitacora/estadisticas.routes.js
+++ src/modules/bitacora/estadisticas.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getHorasPorCarrera, getUsoLaboratorios, getHorasDocentePorMateria, getHorasMateriaPorPeriodo, getUsoLaboratorioDetallado } from './estadisticas.controller.js';
 const router = Router();
+import authMiddleware from "../../middlewares/auth.middleware.js";
 
-router.get('/carreras', getHorasPorCarrera);
-router.get('/laboratorios', getUsoLaboratorios);
-router.get('/docente/:maestro_id', getHorasDocentePorMateria);
-router.get('/materia/:materia', getHorasMateriaPorPeriodo);
-router.get('/laboratorio/:laboratorio_id/detalles', getUsoLaboratorioDetallado);
+router.get('/carreras', authMiddleware, getHorasPorCarrera);
+router.get('/laboratorios', authMiddleware, getUsoLaboratorios);
+router.get('/docente/:maestro_id', authMiddleware, getHorasDocentePorMateria);
+router.get('/materia/:materia', authMiddleware, getHorasMateriaPorPeriodo);
+router.get('/laboratorio/:laboratorio_id/detalles', authMiddleware, getUsoLaboratorioDetallado);
 
 export default router;
